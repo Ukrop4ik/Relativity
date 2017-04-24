4b9ea95a81138920d5da691ef6b8606772d35e78
andrey.zaycev <dondrakone89@mail.ru>
2017-04-25 01:13:03 +0300

    PortreitUI lvl 1,2,3

---
+++ Assets/ExitToMainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitToMainMenu : MonoBehaviour {
+
+    public GameObject panelYesNo;
+
+    public void Yes()
+    {
+        Time.timeScale = 1;
+        GameObject profile = GameObject.Find("Profile");
+        Destroy(profile);
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void NO()
+    {
+        Time.timeScale = 1;
+        panelYesNo.SetActive(false);
+    }
+
+    public void Click()
+    {
+        Time.timeScale = 0;
+        panelYesNo.SetActive(true);
+    }
+}
