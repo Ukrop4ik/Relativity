98a923fca8b111841d870e21cc1d30fd53b5ba77
andrey.zaycev <dondrakone89@mail.ru>
2017-05-14 22:06:09 +0300

    ADS

---
+++ Assets/Script/UnityAdsExample.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class UnityAdsExample : MonoBehaviour
+{
+
+    void Start()
+    {
+        Invoke("ShowAd", 4f);
+    }
+    public void ShowAd()
+    {
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show();
+        }
+    }
+
+
+}
