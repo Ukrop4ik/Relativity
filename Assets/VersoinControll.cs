b06640cacdc0d54bd0704dc887bbfb9eb921f9ac
andrey.zaycev <dondrakone89@mail.ru>
2017-05-20 16:48:47 +0300

    Version

---
+++ Assets/VersoinControll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class VersoinControll : MonoBehaviour {
+
+    public int version;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("version") != version)
+        {
+
+            if (File.Exists(Application.persistentDataPath + "/" + "RelatyvityProfile" + ".json"))
+            {
+                File.Delete(Application.persistentDataPath + "/" + "RelatyvityProfile" + ".json");
+            }
+
+            PlayerPrefs.SetInt("skill_value", 0);
+            PlayerPrefs.SetInt("version", version);
+            PlayerPrefs.Save();
+
+
+        }
+    }
+}
